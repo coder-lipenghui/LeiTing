80c5805de607fb95fa5cf63b5fe1ade21c1f1e40
lipenghui <3464285@gmail.com>
2026-05-12 16:53:44 +0800

    直升飞机机翼旋转

---
+++ Assets/Scripts/Effects/PropellerRotator.cs
@@ -0,0 +1,26 @@
+using LeiTing.Core;
+using UnityEngine;
+
+namespace LeiTing.Effects
+{
+    [DisallowMultipleComponent]
+    public class PropellerRotator : MonoBehaviour
+    {
+        [SerializeField] private float degreesPerSecond = -1440f;
+        [SerializeField] private bool useUnscaledTime;
+        [SerializeField] private bool spinOnlyDuringGameplay = true;
+
+        private void Update()
+        {
+            if (spinOnlyDuringGameplay
+                && GameManager.Instance != null
+                && GameManager.Instance.CurrentState != GameState.Playing)
+            {
+                return;
+            }
+
+            var deltaTime = useUnscaledTime ? Time.unscaledDeltaTime : Time.deltaTime;
+            transform.Rotate(0f, 0f, degreesPerSecond * deltaTime, Space.Self);
+        }
+    }
+}
